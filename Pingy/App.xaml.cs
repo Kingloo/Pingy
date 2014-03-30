55a46c4fb42433fffd8f33eca312c7958d2967c8
Kingloo <k1ngloo@hotmail.com>
2014-03-30 19:17:45 +0200

    Added logging
    A logfile.txt will appear in Documents directory.

--- Pingy/App.xaml.cs
+++ Pingy/App.xaml.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace Pingy
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            string logFilePath = string.Format("C:\\Users\\{0}\\Documents\\logfile.txt", Environment.UserName);
+
+            using (StreamWriter sw = new StreamWriter(logFilePath))
+            {
+                sw.WriteLine(string.Format("\n{0}: {1}: {2}: {3}\n", DateTime.Now, Application.Current.ToString(), e.Exception.ToString(), e.Exception.Message.ToString()));
+            }
+
+            e.Handled = true;
+        }
     }
 }
